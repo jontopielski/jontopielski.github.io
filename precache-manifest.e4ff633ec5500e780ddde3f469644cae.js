09e110e1c0e50ea9ce415101c7d8ccacfa6cfc43
Jon Topielski <jontopielski@Jons-MacBook-Pro.local>
2021-07-04 21:12:22 -0700

    Go back to old personal site

---
+++ precache-manifest.e4ff633ec5500e780ddde3f469644cae.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59c90da8a2830e9748280546e4b1be77",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2435a148aa62c0670635",
+    "url": "/static/css/main.4eb499bd.chunk.css"
+  },
+  {
+    "revision": "e19b323f401669313f63",
+    "url": "/static/js/2.f856eadb.chunk.js"
+  },
+  {
+    "revision": "1af2e467dd1dd801ef7041afe25404ab",
+    "url": "/static/js/2.f856eadb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2435a148aa62c0670635",
+    "url": "/static/js/main.e7c7f1dd.chunk.js"
+  },
+  {
+    "revision": "766df7dbb5392e1314ed",
+    "url": "/static/js/runtime-main.9c9c41ef.js"
+  },
+  {
+    "revision": "f837c4fe1a16ac5522747fab32fbaaf7",
+    "url": "/static/media/DitherCat.f837c4fe.png"
+  },
+  {
+    "revision": "abd8dbe957c0d65f6cff8413d94de1d6",
+    "url": "/static/media/GameCurator.abd8dbe9.png"
+  },
+  {
+    "revision": "b22cedf9b836fa7f23ad46d60969c0af",
+    "url": "/static/media/NewGrounds_Logo.b22cedf9.png"
+  },
+  {
+    "revision": "9115af7b448f9039a09b5f19201df8d5",
+    "url": "/static/media/Warpdoor.9115af7b.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
